e27ac9ea8e846695b6896033938616074623d9f3
anantraj2808 <anantaditya6@gmail.com>
2023-12-13 14:48:45 +0530

    bottom navigation fixed

--- lib/bottom_navigation.dart
+++ lib/bottom_navigation.dart
@@ -1,6 +1,8 @@
 import 'package:chargemod_assignment/features/HomePage/Widgets/home_page.dart';
 import 'package:chargemod_assignment/features/ProfilePage/Widgets/profile_page.dart';
+import 'package:chargemod_assignment/utilities/color_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({Key? key}) : super(key: key);
@@ -14,6 +16,8 @@ class _BottomNavigationState extends State<BottomNavigation> {
   int _selectedIndex = 0;
   List<Widget> widgetOptions = <Widget>[
     HomePage(),
+    Container(),
+    Container(),
     ProfilePage(),
   ];
 
@@ -30,18 +34,28 @@ class _BottomNavigationState extends State<BottomNavigation> {
         child: widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        showUnselectedLabels: true,
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: SvgPicture.asset("assets/images/home_icon.svg", color: _selectedIndex == 0 ? ColorConstants.chargemodOrange : ColorConstants.chargemodBlack,),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: SvgPicture.asset("assets/images/activity_icon.svg", color: _selectedIndex == 1 ? ColorConstants.chargemodOrange : ColorConstants.chargemodBlack,),
+            label: 'Activity',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/images/community_icon.svg", color: _selectedIndex == 2 ? ColorConstants.chargemodOrange : ColorConstants.chargemodBlack,),
+            label: 'Community',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/images/profile_icon.svg", color: _selectedIndex == 3 ? ColorConstants.chargemodOrange : ColorConstants.chargemodBlack,),
+            label: 'Profile',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
+        unselectedItemColor: ColorConstants.chargemodBlack,
         onTap: _onItemTapped,
       ),
     );
